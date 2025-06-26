eba16b7c28250b08ca42f9f9cc07b5aabefaefe9
Morozova Dilyara <dmorozova1403@gmail.com>
2025-06-27 00:51:00 +0300

    Update 5script.js

--- 5script.js
+++ 5script.js
@@ -1,25 +1,21 @@
-const swiper = new Swiper('.swiper', {   // свайпер
-    // Основные настройки
-    direction: 'horizontal',
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
     loop: true,
-  
-    // Пагинация
+    
     pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
 });
 
-const showMoreBtn = document.querySelector('.slider-text');    // сохранение кнопки 
-const brandsMore = document.querySelector('.slider-container__unvisible-brands'); // сохранение класса с брендами
-
-showMoreBtn.addEventListener('click', ()=> {             // обработчик событий на кпопку
-brandsMore.classList.toggle('visible');                   // Переключение класса с flex
-
-    if ( brandsMore.classList.contains('visible')) {         // Проверка наличия класса 
-        showMoreBtn.textContent = 'Скрыть';
-        brandsMore.classList.add ('.slider-container__visible-brands');    // Добавляет класс c none
-    } else  {
-        showMoreBtn.textContent = 'Показать все';
+const showMoreBtn = document.querySelector(".slider-text");
+const brandsMore = document.querySelector(".slider-container__unvisible-brands");
+showMoreBtn.addEventListener("click", () => {
+    brandsMore.classList.toggle("visible");
+    if (brandsMore.classList.contains("visible")) {
+        showMoreBtn.textContent = "Скрыть";
+        brandsMore.classList.add(".slider-container__visible-brands");
+    } else {
+        showMoreBtn.textContent = "Показать все";
     }
 });
